d99bb80c710a0944bbec1ece575dbc0c2a9f57f4
Perkles <hiury_jp@hotmail.com>
2021-09-28 13:21:28 -0300

    DesafioConcluido-DIO-Dividir-X-Por-Y

---
+++ DesafioDividirXporY/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DesafioDividirXporY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                double x = double.Parse(line[0]);
+                double y = double.Parse(line[1]);
+
+                if (y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    double divisao = x / y;
+                    Console.WriteLine(divisao.ToString("N1"));
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
